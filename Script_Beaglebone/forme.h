b7cff569e95b4b7abb4ff424933f0b25757715ad
killiannguyen <killian.nguyen@hotmail.fr>
2016-03-23 23:53:15 +0100

    Create forme.h

---
+++ Script_Beaglebone/forme.h
@@ -0,0 +1,14 @@
+#ifndef FORME_H
+#define FORME_H
+
+#include<iostream>
+#include<stdio.h>
+#include<unistd.h>
+
+using namespace std;
+
+void carre(int boucle);
+void triangle(int boucle);
+void cercle(int boucle);
+
+#endif
