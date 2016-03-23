0a652ce6d549e748855a9111104124fedeca2fcb
killiannguyen <killian.nguyen@hotmail.fr>
2016-03-23 23:53:40 +0100

    Create led.h

---
+++ Script_Beaglebone/led.h
@@ -0,0 +1,13 @@
+#ifndef LED_H  
+#define LED_H
+
+#include<iostream>
+#include<stdio.h>
+#include<unistd.h>
+
+using namespace std;
+
+void LED_BB(int i);
+void LED_VERTE(int i);
+void LED_ROUGE(int i);
+#endif
